c1c7d95485144b5026b4660c5eeb5295eb503a77
Myra <myrabagga07@gmail.com>
2026-07-30 17:12:35 -0400

    updated requirement and added excel file read

--- text_cleaner.py
+++ text_cleaner.py
@@ -1,2 +1,9 @@
 import pandas as pd
 from dateutil import parser
+import sys
+df = ""
+try:
+    print("Enter Excel File Path:")
+    df = pd.read_excel(input())
+except:
+    sys.exit("Error: Excel file not found.")
