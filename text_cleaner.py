8de7b81fa9faf30b5ddd775ab5a863d25670c4a8
Myra <myrabagga07@gmail.com>
2026-07-30 17:33:36 -0400

    output dir

--- text_cleaner.py
+++ text_cleaner.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from dateutil import parser
 import sys
+from pathlib import Path
 df = ""
 try:
     print("Enter Excel File Path:")
     df = pd.read_excel(input())
 except:
     sys.exit("Error: Excel file not found.")
+print("Specify transcript output directory:")
+output_path = Path(input())
+output_path.mkdir(parents=True, exist_ok=True)
+print(f"Nested directories '{output_path}' created successfully.")
