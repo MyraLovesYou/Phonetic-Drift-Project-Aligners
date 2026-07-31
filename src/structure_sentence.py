7563c99abc5ac22d3ca425f6be1dd6c9f29b3e50
Myra <myrabagga07@gmail.com>
2026-07-31 17:49:46 -0400

    removed langdetect and implemented carrier phrase insertion

--- src/structure_sentence.py
+++ src/structure_sentence.py
@@ -1,8 +1,22 @@
-from langdetect import detect
+import unicodedata
 CH_START = "我在读"
 CH_END = "这个词"
 EN_START = "I say "
 EN_END = " again"
 
+def is_english_word(word: str) -> bool:
+    return word.isalpha() and word.isascii()
+
 def format_c_phrases(target_words):
     return_string = ""
+    words = target_words.split("|")
+    for word in words:
+        if is_english_word(word):
+            print(f"Detected {word} as en")
+            return_string += EN_START + word + EN_END + " "
+        else:
+            print(f"Detected {word} as cn (or other)")
+            sentence = CH_START + word + CH_END
+            sentence = " ".join(sentence)
+            return_string += sentence + " "
+    return return_string
